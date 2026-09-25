af878a12ad209733c84030559b080af6ba8b23e4
Atharv Tyagi <aadi.tyagi2008@gmail.com>
2026-09-25 16:33:17 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Get a free API key (no card required) at https://aistudio.google.com/apikey
-const GEMINI_API_KEY = "AQ.Ab8RN6Jdgz7cr8Lw7UDgVEtHXXMR9gMZvb8q1U6kssdSQUpGNw";
-const GEMINI_MODEL = "gemini-3.8-flash";
